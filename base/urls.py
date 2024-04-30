e12c2f71c69de711c36b550e12b19322c3408427
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-04-30 11:05:44 +0200

    logout

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, CustomLogoutView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns =[
     path('login/', CustomLoginView.as_view(), name ='login'),
+    path('logout/', CustomLogoutView.as_view(next_page='login'), name='logout'), 
     path('', TaskList.as_view(), name ='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name ='task'),
     path('task-create/', TaskCreate.as_view(), name ='task-create'),
--- base/views.py
+++ base/views.py
@@ -4,7 +4,15 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy 
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+
+class CustomLogoutView(LogoutView):
+    http_method_names = ['get', 'post']  
+
+
 from .models import Task
+
+
 
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
